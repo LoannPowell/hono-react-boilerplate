9bb2751d59efeab9456c016d73b967f8fce8d315
LoannPowell <juansh821@gmail.com>
2025-09-02 02:47:17 -0500

    Fix prod deploy

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,35 +1,36 @@
-import { Hono } from 'hono';
-import { logger } from 'hono/logger';
-import { healthRoutes } from './routes/health';
-import { aiRoutes } from './routes/ai';
-import { authRoutes } from './routes/auth';
-import { serveStatic } from 'hono/bun';
-import { auth } from './lib/auth';
+import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { healthRoutes } from './routes/health'
+import { aiRoutes } from './routes/ai'
+import { authRoutes } from './routes/auth'
+import { serveStatic } from 'hono/bun'
+import { auth } from './lib/auth'
 
-const app = new Hono();
+const app = new Hono()
 
 app.use('*', logger())
-app.use("*", async (c, next) => {
-	const session = await auth.api.getSession({ headers: c.req.raw.headers });
- 
-  	if (!session) {
-    	c.set("user", null);
-    	c.set("session", null);
-    	return next();
-  	}
- 
-  	c.set("user", session.user);
-  	c.set("session", session.session);
-  	return next();
-});
+app.use('*', async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers })
+  if (!session) {
+    c.set('user', null)
+    c.set('session', null)
+    return next()
+  }
+  c.set('user', session.user)
+  c.set('session', session.session)
+  return next()
+})
 
-const apiRoutes = app.basePath('/api')
-      .route('/health',healthRoutes)
-      .route('/ai', aiRoutes)
-      .route('/auth', authRoutes)
+const apiRoutes = app
+  .basePath('/api')
+  .route('/health', healthRoutes)
+  .route('/ai', aiRoutes)
+  .route('/auth', authRoutes)
 
-app.get('*', serveStatic({ root: '/frontend/dist' }))
-app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
-export default app;
+app.use('/assets/*', serveStatic({ root: '/app/apps/api/frontend/dist' }))
+app.get('/', serveStatic({ path: '/app/apps/api/frontend/dist/index.html' }))
+app.get('*', serveStatic({ path: '/app/apps/api/frontend/dist/index.html' }))
 
-export type ApiRoutes = typeof apiRoutes;
+export default app
+export type ApiRoutes = typeof apiRoutes
+
